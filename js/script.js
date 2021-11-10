72dbfc0ae480afb9b5a6399b333bd228604b3144
youshyna <70101754+youshyna@users.noreply.github.com>
2021-11-10 14:16:52 +0200

    one page scroll

--- js/script.js
+++ js/script.js
@@ -1,1 +1,52 @@
+window.onload = () => {
+  const Slider = function(pages, pagination) {
+    let slides = [],
+        btns = [],
+        count = 0,
+        current = 0,
+        touchstart = 0,
+        animation_state = false;
+
+    const init = () => {
+      slides = pages.children;
+      count = slides.length;
+      for(let i = 0; i < count; i++) {
+        slides[i].style.bottom = -(i * 100) + '%';
+        let btn = document.createElement('li');
+        btn.dataset.slide = i;
+        btn.addEventListener('click', btnClick)
+        btns.push(btn);
+        pagination.appendChild(btn);
+      }
+      btns[0].classList.add('active');
+    }
+
+    const gotoNum = (index) => {
+      if((index != current) && !animation_state) {
+        animation_state = true;
+        setTimeout(() => animation_state = false, 500);
+        btns[current].classList.remove('active');
+        current = index;
+        btns[current].classList.add('active');
+        for(let i = 0; i < count; i++) {
+          slides[i].style.bottom = (current - i) * 100 + '%';
+        }
+      }
+    }
+
+    const gotoNext = () => current < count - 1 ? gotoNum(current + 1) : false;
+    const gotoPrev = () => current > 0 ? gotoNum(current - 1) : false;
+    const btnClick = (e) => gotoNum(parseInt(e.target.dataset.slide));
+    pages.ontouchstart = (e) => touchstart = e.touches[0].screenY;
+    pages.ontouchend = (e) => touchstart < e.changedTouches[0].screenY ? gotoPrev() : gotoNext();
+    pages.onmousewheel = pages.onwheel = (e) => e.deltaY < 0 ? gotoPrev() : gotoNext();
+
+    init();
+  }
+
+  let pages = document.querySelector('.pages');
+  let pagination = document.querySelector('.pagination');
+  let slider = new Slider(pages, pagination)
+}
+
 
